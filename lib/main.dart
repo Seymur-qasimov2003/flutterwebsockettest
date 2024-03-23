6c5e5b00ac17a0edccbaca7a4161525a2068fde6
s.qasimov <seymur.qasimov@btarge.com>
2024-03-24 00:47:16 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false, home: WebsocketDemo()));
+}
+
+class WebsocketDemo extends StatefulWidget {
+  const WebsocketDemo({Key? key}) : super(key: key);
+
+  @override
+  State<WebsocketDemo> createState() => _WebsocketDemoState();
+}
+
+class _WebsocketDemoState extends State<WebsocketDemo> {
+  String btcUsdtPrice = "";
+  String name = "";
+  final channel = IOWebSocketChannel.connect(
+      'wss://stream.binance.com:9443/ws/btcusdt@trade');
+  @override
+  void initState() {
+    super.initState();
+    streamListener();
+  }
+
+  streamListener() {
+    channel.stream.listen((message) {
+      // channel.sink.add('received!');
+      // channel.sink.close(status.goingAway);
+      var getData = jsonDecode(message);
+      setState(() {
+        btcUsdtPrice = getData['p'];
+        name = getData['s'];
+      });
+      // print(getData['p']);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$name Price",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 30),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                btcUsdtPrice,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 250, 194, 25),
+                    fontSize: 30),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
